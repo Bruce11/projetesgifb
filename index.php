49ea6e8eedf84e749971004a8efcfc5000adcf4d
Bruce11 <bruce_sultan@hotmail.com>
2015-04-07 15:13:36 +0200

    add fb php sdk

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
-	const APPID = "1431531240480284"
-	const APPSECRET = "08611dedf4fdc28a5a02f465a14f6aec"
+	const APPID = "1431531240480284";
+	const APPSECRET = "08611dedf4fdc28a5a02f465a14f6aec";
 ?>
 
 <!DOCTYPE html>
@@ -8,35 +8,37 @@
 	<head>
 		<meta charset="utf-8">
 		<title>Concours Photo ESGI</title>
-		<meta name="description" content="Concours Photo" />
+		<meta name="description" content="contenu de ma page" />
+	</head>	
 		
-		
-		<body>
-			<script>
-			  window.fbAsyncInit = function() {
-				FB.init({
-				  appId      : '1431531240480284',
-				  xfbml      : true,
-				  version    : 'v2.3'
-				});
-			  };
+	<body>
+		<script>
+		  window.fbAsyncInit = function() {
+			FB.init({
+			  appId      : '1431531240480284',
+			  xfbml      : true,
+			  version    : 'v2.3'
+			});
+		  };
 
-			  (function(d, s, id){
-				 var js, fjs = d.getElementsByTagName(s)[0];
-				 if (d.getElementById(id)) {return;}
-				 js = d.createElement(s); js.id = id;
-				 js.src = "//connect.facebook.net/en_US/sdk.js";
-				 fjs.parentNode.insertBefore(js, fjs);
-			   }(document, 'script', 'facebook-jssdk'));
-			</script>	
-			
-			<div
-			  class="fb-like"
-			  data-share="true"
-			  data-width="450"
-			  data-show-faces="true">
-			</div>
-			
-		</body>		
-	</head>
+		  (function(d, s, id){
+			 var js, fjs = d.getElementsByTagName(s)[0];
+			 if (d.getElementById(id)) {return;}
+			 js = d.createElement(s); js.id = id;
+			 js.src = "//connect.facebook.net/en_US/sdk.js";
+			 fjs.parentNode.insertBefore(js, fjs);
+		   }(document, 'script', 'facebook-jssdk'));
+		</script>	
+		
+		<h1>Hello World</h1>
+		
+		<div
+		  class="fb-like"
+		  data-share="true"
+		  data-width="450"
+		  data-show-faces="true">
+		</div>
+		
+	</body>		
+	
 </html>
