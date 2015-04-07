50692ec69a364e6cab608f6c6a39bf60551c54c7
Bruce11 <bruce_sultan@hotmail.com>
2015-04-07 16:33:25 +0200

    add

--- index.php
+++ index.php
@@ -1,17 +1,31 @@
 <?php
+    error_reporting(E_ALL);
+    ini_set("display_error",1);
 
-	require "facebook-php-sdk-v4-4.0-dev/autoload.php"
-
+    $session_start();
+	require "facebook-php-sdk-v4-4.0-dev/autoload.php";
 	use Facebook\FacebookSession;
+	use Facebook\FacebookRedirectLoginHelper;
 	
 	const APPID = "1431531240480284";
 	const APPSECRET = "08611dedf4fdc28a5a02f465a14f6aec";
-	
+    const WEBURL = "https://projetesgifb.herokuapp.com/";
+
 	FacebookSession::setDefaultApplication(APPID, APPSECRET);
+	$helper = new FacebookRedirectLoginHelper(WEBURL);
+
+	//$loginUrl = $helper->getLoginUrl();
+
+    //$session = $helper->getSessionFromRedirect();
+    if( isset($_SESSION) && isset($_SESSION['fb_token']) ){
+        $session = new FacebookSession($_SESSION['fb_token']);
+    } else {
+        $session = $helper->getSessionFromRedirect();
+    }
 ?>
 
 <!DOCTYPE html>
-<html>
+<html lang="fr">
 	<head>
 		<meta charset="UTF-8">
 		<title>Concours Photo ESGI</title>
@@ -19,6 +33,16 @@
 	</head>	
 		
 	<body>
+
+        <?php
+        if($session){
+            $_SESSION['fb_token'] = (string) $session->getAccessToken();
+        } else {
+            $loginUrl = $helper->getLoginUrl();
+            echo "<a href='".$loginUrl."'>Facebook Login</a>";
+        }
+        ?>
+
 		<script>
 		  window.fbAsyncInit = function() {
 			FB.init({
